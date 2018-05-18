97f54a629670563dd6d3656b5b973d8db0bb6b75
Kumaresan Shanmugam <kumaresan.s@TYCHONINDIA.NET>
2018-05-18 20:32:20 +0530

    User Models and Routes

--- models/user.js
+++ models/user.js
@@ -2,3 +2,24 @@ const mongoose = require('mongoose');
 const bcryptjs = require('bcryptjs');
 const config = require('../database/config');
 
+//user models
+
+const   UserSchema = mongoose.Schema({
+
+   name:{
+       type:string
+   },
+   email:{
+       type:string,
+       required:true
+   },
+   username:{
+       type:string,
+       required:true
+   },
+   password:{
+        type:string,
+        required:true
+   }
+
+});
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,7 @@ const express = require('express');
 
 //App router
 const router = express.Router();
-
-
-        
+ 
 router.post('/register',(req,res,next)=>{
     res.send('register page');
 });
