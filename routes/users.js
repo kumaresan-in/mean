98455b5fd0aadba05c0b2fe4e965d981b2f5a78d
Kumaresan Shanmugam <kumaresan.s@TYCHONINDIA.NET>
2018-05-21 13:02:01 +0530

    User Routes Updated

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,28 @@
 const express = require('express');
-
-//App router
 const router = express.Router();
-
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
-
 const User = require('../models/user');
  
-router.post('/register',(req,res,next)=>{
-    res.send('register page');
+
+router.post('/register',(req, res, next)=>{
+    //res.send('register page');
+    let newUser = new User({
+            name:req.body.name,
+            email:req.body.email,
+            username:req.body.username,
+            password:req.body.password
+    });
 });
+
+User.addUser(newUser,(err,user)=>{
+    if(err){
+        res.json({success:false,mgs:'failed to register user'});
+    }
+    else{
+        res.json({success:true,mgs:'user registerd'});
+    }
+})
 
 router.get('/authenticate',(req,res,next)=>{
     res.send('authenticate page');
